7f569910ad7db1e599de8987303ae68925a44bd0
Ashish Madhup <119279720+madhupashish@users.noreply.github.com>
2024-01-18 18:10:14 +0530

    Update Video.js

--- backend/models/Video.js
+++ backend/models/Video.js
@@ -1,40 +1,61 @@
 import mongoose from "mongoose";
 
-const VideoSchema=new mongoose.Schema({
-    userId:{
-        type:String,
-        required:true,
+const VideoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId, 
+      required: true,
     },
-    title:{
-        type:String, 
-        required:true,   
+    title: {
+      type: String,
+      required: true,
+      trim: true, 
     },
-    description:{
-        type:String,
-        required:true,
+    description: {
+      type: String,
+      required: true,
+      maxLength: 500, 
     },
-    imgUrl:{
-        type:String,
-        required:true,
+    imgUrl: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value) => {
+          return /^(https?|ftp):\/\/.*(jpeg|jpg|png|gif|bmp)$/.test(value);
+        },
+        message: "Invalid imgUrl format",
+      },
     },
-    videoUrl:{
-        type:String,
-        required:true,
+    videoUrl: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (value) => {
+          return /^(https?|ftp):\/\/.*(mp4|avi|mkv|mov)$/.test(value);
+        },
+        message: "Invalid videoUrl format",
+      },
     },
-    views:{
-        type:Number,
-        default:0
+    views: {
+      type: Number,
+      default: 0,
     },
-    likes:{
-        type:[String],
-        default:[],
-    },
-    dislikes:{
-        type:[String],
-        default:[],
-    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
+      },
+    ],
+    dislikes: [
+      {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
+VideoSchema.index({ userId: 1, title: 1 }, { unique: true });
 
-},{timestamps:true});
-
-export default mongoose.model("Video",VideoSchema);
+export default mongoose.model("Video", VideoSchema);
